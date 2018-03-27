022ff9b6a103c7f286616fb0aa2c202eecc297a7
Aaron Williams <aaron.colby.williams@gmail.com>
2018-03-27 00:44:03 -0400

    v1 of site design

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -2,13 +2,15 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 
-import Navbar from '../components/Navbar'
 import './style.scss'
 
 const TemplateWrapper = ({ children }) => (
   <div>
-    <Helmet title="Home | Aaron Colby Williams" />
-    <Navbar />
+    <Helmet>
+      <title>Home | Aaron Colby Williams</title>
+      <link href="https://fonts.googleapis.com/css?family=Open+Sans|Righteous" rel="stylesheet" />
+      <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.8/css/all.css" integrity="sha384-3AB7yXWz4OeoZcPbieVW64vVXEwADiYyAEhwilzWsLw+9FgqpyjjStpPnpBO8o8S" crossorigin="anonymous" />
+    </Helmet>
     <div>{children()}</div>
   </div>
 )
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,42 +2,77 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Link from 'gatsby-link'
 
+import profilePhoto from '../img/me.jpg'
+
 export default class IndexPage extends React.Component {
   render() {
     const { data } = this.props
     const { edges: posts } = data.allMarkdownRemark
 
     return (
-      <section className="section">
-        <div className="container">
-          <div className="content">
-            <p> Aaron Williams is a reporter who specializes in graphics and data visualization for The Washington Post. Before joining The Post, he covered housing, campaign finance, police and local politics for the San Francisco Chronicle and the Center for Investigative Reporting.</p>
+      <section className="container">
+        <div className="columns">
+          <div>
+            <figure className="image" id="profile">
+              <img src={profilePhoto} alt="Aaron Colby Williams" />
+            </figure>
           </div>
-          {/* {posts
-            .filter(post => post.node.frontmatter.templateKey === 'blog-post')
-            .map(({ node: post }) => (
-              <div
-                className="content"
-                style={{ border: '1px solid #eaecee', padding: '2em 4em' }}
-                key={post.id}
-              >
-                <p>
-                  <Link className="has-text-primary" to={post.fields.slug}>
-                    {post.frontmatter.title}
-                  </Link>
-                  <span> &bull; </span>
-                  <small>{post.frontmatter.date}</small>
-                </p>
-                <p>
-                  {post.excerpt}
-                  <br />
-                  <br />
-                  <Link className="button is-small" to={post.fields.slug}>
-                    Keep Reading →
-                  </Link>
-                </p>
-              </div>
-            ))} */}
+          <div>
+          <section className="hero">
+            <div className="hero-body">
+              
+                <h1 className="title">
+                  Aaron Colby Williams
+                </h1>
+                <p>I'm a reporter who specializes in graphics and data visualization for <a href="https://www.washingtonpost.com/people/aaron-williams/">The Washington Post</a>. Before joining The Post, I covered housing, campaign finance, police and local politics for the San Francisco Chronicle and the Center for Investigative Reporting.</p>
+
+                <div className="social">
+                  <a href="https://twitter.com/aboutaaron" title="Twitter">
+                    <span className="icon">
+                      <i className="fab fa-twitter"></i>
+                    </span>
+                  </a>
+                  <a href="https://github.com/aboutaaron" title="Github">
+                    <span className="icon">
+                      <i className="fab fa-github"></i>
+                    </span>
+                  </a>
+                  <a href="https://www.linkedin.com/in/aboutaaron" title="LinkedIn">
+                    <span className="icon">
+                      <i className="fab fa-linkedin"></i>
+                    </span>
+                  </a>
+                </div>
+            </div>
+          </section>
+          </div>
+          
+            
+            {/* {posts
+              .filter(post => post.node.frontmatter.templateKey === 'blog-post')
+              .map(({ node: post }) => (
+                <div
+                  className="content"
+                  style={{ border: '1px solid #eaecee', padding: '2em 4em' }}
+                  key={post.id}
+                >
+                  <p>
+                    <Link className="has-text-primary" to={post.fields.slug}>
+                      {post.frontmatter.title}
+                    </Link>
+                    <span> &bull; </span>
+                    <small>{post.frontmatter.date}</small>
+                  </p>
+                  <p>
+                    {post.excerpt}
+                    <br />
+                    <br />
+                    <Link className="button is-small" to={post.fields.slug}>
+                      Keep Reading →
+                    </Link>
+                  </p>
+                </div>
+              ))} */}
         </div>
       </section>
     )
